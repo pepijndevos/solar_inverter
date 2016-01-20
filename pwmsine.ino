1b2b139013f699c86f2fb881ce756ac7cc3882a3
Pepijn de Vos <pepijndevos@gmail.com>
2016-01-20 15:43:09 +0100

    updates

--- pwmsine.ino
+++ pwmsine.ino
@@ -17,10 +17,10 @@ void setup() {
 
   // Set H-bridge PWM prescale to 1
   TCCR2B&=~0b111;
-  TCCR2B|=1;
+  TCCR2B|=3;
   // Set Boost PWM prescale to 1
   TCCR1B&=~0b111;
-  TCCR1B|=1;
+  TCCR1B|=3;
   // Add 1ms compare interrupt to timer 1
   OCR0A = 0xAF; // Leet AF
   TIMSK0 |= _BV(OCIE0A);
@@ -28,6 +28,7 @@ void setup() {
   Serial.begin(9600);
   digitalWrite(LEDPIN, HIGH);
   analogWrite(BOOSTPIN, 0);
+  //analogWrite(PWMPINNEG, 127);
 }
 
 
@@ -41,25 +42,28 @@ SIGNAL(TIMER0_COMPA_vect) {
   analogWrite((pin ? PWMPINPOS : PWMPINNEG), sine256[idx%COUNT]);
   //analogWrite(PWMPINPOS, sine256[idx]);
   
-  //if(idx%100==0) boost(); // adjust boost voltage every 100ms
   idx = (idx+1)%10;
 }
 
-/*void boost() {
+void boost() {
   static uint8_t i = 0;
   // read the value from the sensor:
   float sensorValue = analogRead(SENSORPIN);
   float voltage = sensorValue*50.0/1024.0;
-  if(voltage>30 && i>1) {
+  if(voltage>15 && i>1) {
     i--;
-  } else if(voltage<30 && i<200) {
+  } else if(voltage<15 && i<200) {
     i++;
   }
-  analogWrite(10, i);
-}*/
+  Serial.println(i);
+  Serial.println(voltage);
+  analogWrite(BOOSTPIN, i);
+}
 
 void loop() {
-  Serial.println("hello world");
+  Serial.println("Hallo wreld");
+  delay(100);
+  boost();
     /*set_sleep_mode(SLEEP_MODE_IDLE);
     sleep_enable();
     sleep_cpu();
